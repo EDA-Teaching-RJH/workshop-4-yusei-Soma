7837b4367d82959f1b145669d9abadba7783b829
yusei-Soma <ys338@kent.ac.uk>
2024-10-18 15:59:17 +0000

    8uh

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,13 @@
+camel_case=input("Enter a camel case valuable: ")
+print("snake case: ", end="")
+for c in  camel_case:
+    if c.islower():
+        print(c, end="")
+    if c.isupper():
+        print("_",c.lower(), end="")
+print()
+
+
+  
+
 
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,17 @@
+price=75
+total_inserted=0
+valid = [5, 10, 20, 50]
 
+while total_inserted<price:
+        coin=int(input("Please insert a coin(5p, 10p, 20p, 50p):"))
+
+        if coin in valid:
+            total_inserted += coin
+        else:
+            print("Invaild coin. Please insert 5p, 10p, 20p, 50p.")
+
+change= total_inserted - price
+if change>0:
+        print(f"Here is your coffee and your change:{change}p.")
+else:
+        print("No change needed. Here is your coffee!")
